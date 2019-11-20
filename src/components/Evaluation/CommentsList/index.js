97300ce2e796ba9486e52e36d98f043cdfd8ac8c
Dawid Kawiak <d.kawiak@selleo.com>
2019-11-20 08:51:40 +0100

    Add state

--- src/components/Evaluation/CommentsList/index.js
+++ src/components/Evaluation/CommentsList/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { connect } from 'react-redux'
 
 import { fetchPostComments } from '../../../store/comments/actions'
@@ -6,12 +6,19 @@ import ListItem from '../../../components/ListItem'
 import CommentsListStyles from './CommentsListStyles'
 
 export const CommentsList = ({ fetchPostComments, comments }) => {
+  const [count, setCount] = useState(0)
+
   useEffect(() => {
     fetchPostComments()
   }, [])
 
+
   return (
     <>
+      <p>Click counter: {count}</p>
+      <button onClick={() => setCount(count + 1)}>
+        Click to update state
+      </button>
       <h2>Comments</h2>
       <CommentsListStyles>
         {comments.map(comment => <ListItem key={comment.id}>{comment.name}</ListItem>)}
