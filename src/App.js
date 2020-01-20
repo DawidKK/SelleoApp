d131e5a02bd9e3a2b5231a4d43a54cf487079bc5
Dawid Kawiak <d.kawiak@selleo.com>
2020-01-20 18:38:55 +0100

    Add oauth

--- src/App.js
+++ src/App.js
@@ -1,7 +1,46 @@
 import React from 'react';
+import Navbar from './components/Navbar';
+import PrivateRoute from './components/PrivateRoute';
 
-const App = () => {
-  return <div>React</div>;
-};
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+import comments from './store/comments';
+
+import { ThemeProvider } from 'styled-components';
+import Theme from './components/UI/Theme';
+import Inner from './components/UI/Inner';
+import GlobalStyle from './components/UI/GlobalStyles';
+
+import ScreensEventForm from './screens/Event/Form';
+import ScreensCommentsList from './screens/Evaluation/';
+import ScreensUserBoard from './screens/User';
+
+import { Router, Route, Switch } from 'react-router-dom';
+import Profile from './components/Profile';
+import history from './utils/history';
+
+function App() {
+  const store = createStore(comments, applyMiddleware(thunk));
+  return (
+    <Provider store={store}>
+      <Router history={history}>
+        <ThemeProvider theme={Theme}>
+          <GlobalStyle />
+          <Navbar />
+          <Inner>
+            <Switch>
+              <Route path="/" exact />
+              <PrivateRoute path="/profile" component={Profile} />
+              <PrivateRoute path="/form" component={ScreensEventForm} />
+              <PrivateRoute path="/evaluation" component={ScreensCommentsList} />
+              <PrivateRoute path="/user" component={ScreensUserBoard} />
+            </Switch>
+          </Inner>
+        </ThemeProvider>
+      </Router>
+    </Provider>
+  );
+}
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,12 +4,30 @@ import ScreensRoot from './screens/Root';
 import * as serviceWorker from './serviceWorker';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import comments from './store/comments'
+import comments from './store/comments';
+import { Auth0Provider } from './react-auth0-spa';
+import config from './auth_config.json';
+import history from './utils/history';
+import App from './App'
 
-const store = createStore(
-  comments,
-  applyMiddleware(thunk)
+const store = createStore(comments, applyMiddleware(thunk));
+const onRedirectCallback = appState => {
+  history.push(
+    appState && appState.targetUrl
+      ? appState.targetUrl
+      : window.location.pathname
+  );
+};
+
+ReactDOM.render(
+  <Auth0Provider
+    domain={config.domain}
+    client_id={config.clientId}
+    redirect_uri={window.location.origin}
+    onRedirectCallback={onRedirectCallback}
+  >
+    <App />
+  </Auth0Provider>,
+  document.getElementById("root")
 );
-
-ReactDOM.render(<ScreensRoot store={store} />, document.getElementById('root'));
 serviceWorker.unregister();
