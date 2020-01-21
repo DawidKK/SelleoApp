94137b3658156b4968af706aec1af7c325ddc3bb
Dawid Kawiak <d.kawiak@selleo.com>
2020-01-21 21:40:53 +0100

    Add rcab

--- src/components/Evaluation/UserBoard/index.js
+++ src/components/Evaluation/UserBoard/index.js
@@ -1,28 +1,48 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Can from '../../Can';
 
+import { roles } from '../../../rbac-roles';
 export class UserBoard extends Component {
   state = {
-    name: ''
-  }
-  componentDidMount() {
-    this.fetchUser()
-  }
+    role: 'user'
+  };
 
-  fetchUser = () => {
-    fetch('https://jsonplaceholder.typicode.com/users/1')
-    .then(response => response.json())
-    .then(({name}) => this.setState({name}))
-  }
+  handleChange = e => this.setState({ role: e.target.value})
 
   render() {
-    const { today } = this.props
+    const { role } = this.state;
+
     return (
       <div>
-        <h2>Today is {today}</h2>
-        {this.state.name}
+        <h1>Pretend this is Admin board, no kidding</h1>
+        <h2>You can be anyone you ever dream!</h2>
+        {roles.map(role => (
+          <label for={role.role}>
+            <input id={role.role} onChange={this.handleChange} type="radio" name="role.role" value={role.role} />
+            Do you want be {role.role}?
+          </label>
+        ))}
+        <h3>Who can get access to this page?</h3>
+        <Can
+          role={role}
+          perform="admin-page:visit"
+          yes={() => <p>Everyone can get access to this place</p>}
+        />
+        <h3>What can you do on this page?</h3>
+        <Can
+          role={role}
+          perform="admin-page:edit"
+          yes={() => (
+            <>
+              <p>Feel free to change something</p>
+              <button onClick={() => alert('You are boss here')}>Check this out!</button>
+            </>
+          )}
+          no={() => <p>Hold your horses, you are just poor user, don't touch anything</p>}
+        />
       </div>
-    )
+    );
   }
 }
 
-export default UserBoard
+export default UserBoard;
